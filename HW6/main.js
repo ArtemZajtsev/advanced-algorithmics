3456ab21e6fcc5d8f0050a92b1776100b3cbf19b
Artem Zaitsev <minhiriath94@gmail.com>
2017-10-15 17:41:29 +0300

    word mapping to integers with simple approach and approach from java

--- HW6/main.js
+++ HW6/main.js
@@ -24,16 +24,59 @@ const universalHashing = (data, tableSize) => {
     });
     //let collisions = data.length - hashTable.filter(() => {return true}).length;
     //console.log(`Collisions number: ${collisions}`);
-    return data.length - hashTable.filter(() => {return true}).length;
+    return data.length - hashTable.filter(() => {
+        return true
+    }).length;
 };
 
-const hashingMeasurements = (data,tableSize,repeats) => {
+const hashingMeasurements = (data, tableSize, repeats) => {
     let sum = 0;
-    for(let i=0;i<repeats;i++){
-        sum += universalHashing(data,tableSize)
+    for (let i = 0; i < repeats; i++) {
+        sum += universalHashing(data, tableSize)
     }
-    console.log(sum/repeats);
-    return sum/repeats;
+    console.log(sum / repeats);
+    return sum / repeats;
 };
 
-hashingMeasurements(data,1000,100);
+//hashingMeasurements(data,1000,100);
+
+
+let rawData2 = fs.readFileSync("words.txt", "utf-8");
+let data2 = [];
+rawData2.split(/\n/).forEach((x) => {
+    data2.push(x);
+});
+
+const stringHashCodeFromJava = (string) => {
+    let h = 0, i = 0;
+    if (string.length > 0) {
+        while (i < string.length) {
+            h = (h << 5) - h + string.charCodeAt(i++) | 0;
+        }
+    }
+    return h;
+};
+
+const wordMapping = (data, useSimpleApproach) => {
+    let result = [];
+    if (useSimpleApproach === true) {
+        data.forEach((word) => {
+            let sum = 0;
+            for (let i = 0; i < word.length; i++) {
+                sum += word.charCodeAt(i);
+            }
+            result[sum] = word;
+        });
+        console.log(`Percent of colliding integers in simple: ${((data.length - result.filter(() => {
+            return true
+        }).length) / data.length) * 100}% `);
+    } else {
+        let hashCodes = data.map(stringHashCodeFromJava);
+        let set = new Set(hashCodes);
+        console.log(`Percent of colliding integers in java approach: ${((data.length - set.size) / data.length) * 100}%`);
+    }
+    return result;
+};
+
+wordMapping(data2, true);
+wordMapping(data2, false);
